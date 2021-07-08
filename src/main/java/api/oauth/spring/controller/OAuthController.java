cd28d12f90020949643b48763eb1d8090b5ef04b
Jinhong <xlffm3@gmail.com>
2021-07-08 23:37:54 +0900

    feat: Github Oauth 구현

---
+++ src/main/java/api/oauth/spring/controller/OAuthController.java
@@ -0,0 +1,28 @@
+package api.oauth.spring.controller;
+
+import api.oauth.spring.dto.OauthDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
+
+@RestController
+public class OAuthController {
+
+    @GetMapping("/login")
+    public ResponseEntity<String> lgoin(@RequestParam String code) {
+        if (Objects.isNull(code)) {
+            throw new IllegalStateException("Github 로그인이 실패했습니다.");
+        }
+        RestTemplate restTemplate = new RestTemplate();
+        OauthDto oauthDto = new OauthDto();
+        oauthDto.setCode(code);
+        oauthDto.setClient_id("7e930566ecaa306c71b5");
+        oauthDto.setClient_secret("60f474770c7d6a4498265d7a6a08149f1eae5022");
+        String accessToken = restTemplate.postForObject("https://github.com/login/oauth/access_token", oauthDto, String.class);
+        return ResponseEntity.ok(accessToken);
+    }
+}
---
+++ src/main/java/api/oauth/spring/dto/OauthDto.java
@@ -0,0 +1,35 @@
+package api.oauth.spring.dto;
+
+public class OauthDto {
+
+    private String client_id;
+    private String client_secret;
+    private String code;
+
+    public OauthDto() {
+    }
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getClient_secret() {
+        return client_secret;
+    }
+
+    public void setClient_secret(String client_secret) {
+        this.client_secret = client_secret;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
