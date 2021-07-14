731ee6aaacc7f90c455365f0fd318977c17bfe03
Jinhong <xlffm3@gmail.com>
2021-07-15 01:14:40 +0900

    refactor: 메서드명 수정

--- src/main/java/api/oauth/spring/controller/OAuthController.java
+++ src/main/java/api/oauth/spring/controller/OAuthController.java
@@ -12,8 +12,8 @@ import java.util.Objects;
 @RestController
 public class OAuthController {
 
-    @GetMapping("/login")
-    public ResponseEntity<String> lgoin(@RequestParam String code) {
+    @GetMapping("/afterlogin")
+    public ResponseEntity<String> afterlogin(@RequestParam String code) {
         if (Objects.isNull(code)) {
             throw new IllegalStateException("Github 로그인이 실패했습니다.");
         }
@@ -21,7 +21,7 @@ public class OAuthController {
         OauthDto oauthDto = new OauthDto();
         oauthDto.setCode(code);
         oauthDto.setClient_id("7e930566ecaa306c71b5");
-        oauthDto.setClient_secret("60f474770c7d6a4498265d7a6a08149f1eae5022");
+        oauthDto.setClient_secret("578679d802bce91380472b3446d4338460f6c06c");
         String accessToken = restTemplate.postForObject("https://github.com/login/oauth/access_token", oauthDto, String.class);
         return ResponseEntity.ok(accessToken);
     }
